30c97c271786ded52f068879a3a0b6fb1555e63d
Massimiliano Montaleone <theplayergame97@gmail.com>
2020-04-07 19:35:49 +0200

    fix import

--- app/src/main/java/com/fast0n/wifiview/MainActivity.java
+++ app/src/main/java/com/fast0n/wifiview/MainActivity.java
@@ -26,6 +26,7 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
+import com.google.zxing.client.android.BuildConfig;
 import com.google.zxing.common.BitMatrix;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
